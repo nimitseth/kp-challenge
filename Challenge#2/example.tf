5cb7a402b3b9a7b41e534c8689457a1cea6d745f
Nimit Seth <nimitseth@gmail.com>
2023-03-26 20:05:36 +0530

    Update example.tf

--- Challenge#2/example.tf
+++ Challenge#2/example.tf
@@ -1,3 +1,5 @@
+# This command retrieves metadata about a Google Compute Engine instance using the gcloud command-line tool, curl, and the jq command-line tool, and saves the resulting JSON output to a file named "instance_metadata.json".
+
 resource "null_resource" "instance_metadata" {
   provisioner "local-exec" {
     command = "gcloud compute ssh --zone=${var.zone} ${google_compute_instance.instance.name} --command='curl \"http://metadata.google.internal/computeMetadata/v1/instance/?recursive=true\" -H \"Metadata-Flavor: Google\"' | jq . > instance_metadata.json"
